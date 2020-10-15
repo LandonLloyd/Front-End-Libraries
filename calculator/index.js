654c93be14317c6b5148b48c4128340119ba1d50
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-10-15 14:42:12 -0600

    Update index.js

--- calculator/index.js
+++ calculator/index.js
@@ -1,4 +1,5 @@
 let check = true;
+let check0 = true;
 
 $('#clear').click(function clear(){
   $('#display').html('0');
@@ -6,15 +7,27 @@ $('#clear').click(function clear(){
 });
 
 function addDisplay(num){
+  
+  if(num === 0 && check0 === true && $('#display').text().startsWith('0')){
+    $('#display').text('0');
+    check0 = false;
+  }
+  
   if(check === true){
     $('#display').html('');
   }
+  
   $('#display').append(num);
   check = false;
+  
+  
 }
 
 function answer(){
+ 
   let value = eval($('#display').html());
   $('#display').html(value);
   check = true;
 }
+
+
